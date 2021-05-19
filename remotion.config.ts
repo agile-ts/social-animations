18345e0f96613ecab435aa1b44ce403cb7ec7d0f
Benno Kohrs <bennokohrs@gmail.com>
2021-05-19 09:09:46 +0200

    updated config to render only with one core

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,3 +1,4 @@
 import {Config} from 'remotion';
 
 Config.Rendering.setImageFormat('jpeg');
+Config.Rendering.setConcurrency(1); // TODO fix multi core rendering in 'CodeFrame'.. as a workaround only render with one core for now
